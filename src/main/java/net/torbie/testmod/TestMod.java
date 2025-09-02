b153ad1d15f279aa6530ec66ad106f3d8440a38a
Jack <jack@phthalo.net>
2025-09-02 16:34:53 -0400

    Test

---
+++ src/main/java/net/torbie/testmod/TestMod.java
@@ -0,0 +1,17 @@
+package net.torbie.testmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+ // very important
+//lol
+public class TestMod implements ModInitializer {
+	public static final String MOD_ID = "testmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
