1107670883a2e4c12d1e16ff58fa1b3d72d5c410
Jack <jack@phthalo.net>
2025-09-02 23:25:19 -0400

    Created a block texture and added it into the game

--- src/main/java/net/torbie/testmod/TestMod.java
+++ src/main/java/net/torbie/testmod/TestMod.java
@@ -2,6 +2,7 @@ package net.torbie.testmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.torbie.testmod.block.ModBlocks;
 import net.torbie.testmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,7 @@ public class TestMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
         ModItems.registerModItems();
+        ModBlocks.registerModBlocks();
 
 	}
 }
---
+++ src/main/java/net/torbie/testmod/block/ModBlocks.java
@@ -0,0 +1,47 @@
+package net.torbie.testmod.block;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+import net.torbie.testmod.TestMod;
+
+public class ModBlocks {
+
+    public static final Block CHEESE_BLOCK = registerBlock("cheese_block",
+            AbstractBlock.Settings.create().strength(3f, 4f)
+                    .sounds(BlockSoundGroup.MUD));
+
+
+    private static Block registerBlock(String name, AbstractBlock.Settings blockSettings) {
+        RegistryKey <Block> key = RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(TestMod.MOD_ID,name));
+        Block block = new Block(blockSettings.registryKey(key));
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK,key,block);
+    }
+
+    private static void registerBlockItem(String name, Block block) {
+        RegistryKey <Item> key = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(TestMod.MOD_ID,name));
+        BlockItem item = new BlockItem(block, new Item.Settings().registryKey(key));
+        Registry.register(Registries.ITEM,key,item);
+    }
+
+
+
+
+
+    public static void registerModBlocks() {
+        TestMod.LOGGER.info("Registering Mod Blocks for " + TestMod.MOD_ID);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
+            entries.add(CHEESE_BLOCK);
+        });
+    };
+}
